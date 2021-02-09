e838d8d621f4a40108022a20baf45d7ee449d90c
Thomas Cardon <thomas.cardon@pm.me>
2021-02-09 11:04:10 +0100

    Exemple index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Remote Toolbox</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Remote Toolbox
         </h1>
 
         <p className={styles.description}>
