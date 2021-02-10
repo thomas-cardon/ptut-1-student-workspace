4c24b5395e96d95cd845b056ace20b99574d422d
Thomas Cardon <thomas.cardon@pm.me>
2021-02-11 00:17:46 +0100

    /api/posts/recent -> ajout

---
+++ pages/api/posts/recent.js
@@ -0,0 +1,31 @@
+import { withIronSession } from 'next-iron-session';
+import { query } from '../../../lib/db';
+
+async function handler(req, res, session) {
+  const user = req.session.get('user');
+  if (!user || user.userType == 0) return res.status(401).send('NOT_AUTHORIZED');
+
+  try {
+    const data = await query(
+      `
+      SELECT * FROM posts
+      ORDER BY date, id DESC
+      LIMIT 1, 10
+      `
+    );
+
+    res.send({ data, success: true });
+  }
+  catch (e) {
+    res.status(500).json({ message: e.message, success: false });
+  }
+}
+
+export default withIronSession(handler, {
+  cookieName: "ptut-1/sessions/v1",
+  password: process.env.SECRET_COOKIE_PASSWORD,
+  // La sécurisation s'active que si Node.js n'est pas en mode développement (il est en production quand c'est Vercel qui l'héberge)
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+});
