368dca93d7df85c819f146b836b2b1c128666b15
Thomas Cardon <thomas.cardon@pm.me>
2021-02-10 19:20:04 +0100

    /api/me/user -> fixes

--- pages/api/me/user.js
+++ pages/api/me/user.js
@@ -1,12 +1,12 @@
 import { withIronSession } from "next-iron-session";
 
-function handler(req, res, session) {
+function handler(req, res) {
   if (!req.session) return res.status(401).send({ error: 'NOT_AUTHORIZED', success: false });
 
   const user = req.session.get('user');
   if (!user) return res.status(401).send({ error: 'NOT_AUTHORIZED', success: false });
 
-  res.send({ user });
+  res.send(user);
 }
 
 export default withIronSession(handler, {
