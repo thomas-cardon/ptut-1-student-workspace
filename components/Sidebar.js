98b2a94fd3e9a9f761e8638faa5b07c5d8d3b303
Thomas Cardon <thomas.cardon@pm.me>
2021-02-11 05:14:21 +0100

    Tentatives avant grand changements?

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,19 +5,21 @@ import styles from './Sidebar.module.css';
 
 import Badge from './Badge';
 
+import { HiOutlineAdjustments, HiCalendar, HiCollection } from "react-icons/hi";
+
 export default function Sidebar({ title, children }) {
   return (
     <div className={styles.main}>
       <h3 className={styles.title}>Menu</h3>
       <Link href="/posts">
-        <a className={styles.link}>Derniers posts <Badge>1</Badge></a>
+        <a className={styles.link}><Badge>1</Badge>&nbsp;&nbsp;<HiCollection className={styles.icon} />Derniers posts</a>
       </Link>
       <Link href="/schedule">
-        <a className={styles.link}>Emploi du temps <Badge>1</Badge></a>
+        <a className={styles.link}><Badge>1</Badge>&nbsp;&nbsp;<HiCalendar className={styles.icon} />Emploi du temps</a>
       </Link>
       <hr />
       <Link href="/settings">
-        <a className={styles.link}>Paramètres</a>
+        <a className={styles.link}><HiOutlineAdjustments className={styles.icon} />Paramètres</a>
       </Link>
     </div>
   );
