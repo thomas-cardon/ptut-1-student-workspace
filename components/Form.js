6e34066409bdc26cdff2c69b01b392da98f411af
Thomas Cardon <thomas.cardon@pm.me>
2021-02-13 22:42:33 +0100

    Meilleure gestion de formulaires

--- components/Form.js
+++ components/Form.js
@@ -3,12 +3,12 @@ import styles from "./Form.module.css";
 import { useForm, FormProvider } from "react-hook-form";
 import * as Fields from "./FormFields";
 
-export default function Form({ onSubmit, fields, children }) {
+export default function Form({ onSubmit, onError, fields, children }) {
   const { handleSubmit, ...methods } = useForm();
 
   if (!fields) return (
     <FormProvider {...methods}>
-      <form className={styles.form} onSubmit={onSubmit}>
+      <form className={styles.form} onSubmit={onSubmit} onError={onError}>
         {children}
       </form>
     </FormProvider>
@@ -16,7 +16,7 @@ export default function Form({ onSubmit, fields, children }) {
 
   return (
     <FormProvider {...methods}>
-      <form className={styles.form} onSubmit={onSubmit}>
+      <form className={styles.form} onSubmit={onSubmit} onError={onError}>
         {fields.map(({ __typename, ...field }, index) => {
           const Field = Fields[__typename];
           if (!Field) return null;
