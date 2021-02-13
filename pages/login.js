121fe5cb83cb6b6111188417f3da8f46ed47cb33
Thomas Cardon <thomas.cardon@pm.me>
2021-02-13 22:43:28 +0100

    /login réparations

--- pages/login.js
+++ pages/login.js
@@ -14,21 +14,20 @@ import * as Fields from "../components/FormFields";
 
 export default function Login(props) {
   const { user } = useUser({ redirectTo: '/dashboard' }); /* Redirection si l'utilisateur est connecté */
-
-  const loginUser = async event => {
-    event.preventDefault();
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
     try {
       const res = await fetch(location.protocol + '//' + location.host + '/api/me/login', {
         body: JSON.stringify({
-          email: event.target.email.value,
-          password: event.target.password.value
+          email: e.target.email.value,
+          password: e.target.password.value
         }),
         headers: { 'Content-Type': 'application/json' },
         method: 'POST'
       });
 
-      const result = await res.text();
+      const result = await res.json();
       console.dir(result);
 
       if (result.success) Router.push('/dashboard');
@@ -38,11 +37,13 @@ export default function Login(props) {
     }
   }
 
+  const onError = (errors, e) => console.error(errors, e);
+
   return (
     <BasicLayout title="Connexion">
       <h1 className={'title gradient'}>Connectez-vous</h1>
       <h2 className={'title'}>...pour accéder au tableau de bord</h2>
-      <Form onSubmit={loginUser}>
+      <Form onSubmit={onSubmit} onError={onError}>
         <Fields.FormInput label="Adresse mail" id="email" name="email" type="email" placeholder="exemple@exemple.fr" />
         <Fields.FormInput label="Mot de passe" id="password" name="password" type="password" placeholder="Mot de passe difficile à trouver" />
         <Fields.FormButton id="forgotPassword" type="button">Mot de passe oublié?</Fields.FormButton>
