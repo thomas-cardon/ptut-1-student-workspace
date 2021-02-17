b8d5ae0b3ad5789b0b5721a63a672bf3cc94af2b
Thomas Cardon <thomas.cardon@pm.me>
2021-02-17 01:33:01 +0100

    /dashboard -> meilleur chargement

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -8,15 +8,19 @@ export default function Dashboard({ props }) {
   useServiceWorker();
 
   const { user } = useUser({ redirectNotAuthorized: '/login', redirectOnError: '/error' }); /* Redirection si l'utilisateur n'est pas connecté */
-  if (!user) return <div>Loading...</div>
 
-  console.dir(user);
+  let content;
 
+  if (!user) content = <h1 className={'title'}>Chargement...</h1>
+  else content = (
+    <h1 className={'title'}>
+      Salut, {user.firstName} !
+    </h1>
+  );
+  
   return (
     <UserLayout user={user} flex={true}>
-      <h1 className={'title'}>
-        Salut, {user.firstName} !
-      </h1>
+      {content}
     </UserLayout>
   );
 };
