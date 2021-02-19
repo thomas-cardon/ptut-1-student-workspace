c822a174604efbcc808421a014f0f1efa750d452
Thomas Cardon <thomas.cardon@pm.me>
2021-02-20 00:37:36 +0100

    Alertes -> disparition automatique

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ToastProvider>
+    <ToastProvider autoDismiss={true}>
       <Component {...pageProps} />
     </ToastProvider>
   );
