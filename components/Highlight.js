294cd413a09970c4375dfb544adcb61d4953e9ee
Thomas Cardon <thomas.cardon@pm.me>
2021-02-22 18:12:54 +0100

    Highlight -> refactoring

--- components/Highlight.js
+++ components/Highlight.js
@@ -1,9 +1,9 @@
 import styles from './Highlight.module.css';
 
-export default function Sidebar({ user, title = 'Important', children, active, setActive }) {
+export default function Highlight({ title = 'Important', icon = '☝️', children }) {
   return (
     <div className={styles.block}>
-      <div className={styles.icon}>☝️</div>
+      <div className={styles.icon}>{icon}</div>
       <div className={styles.title}>{title}</div>
       {children}
     </div>
