353fb5fa682732e0f275262902a122feec5b550b
Thomas Cardon <thomas.cardon@pm.me>
2021-02-23 05:33:12 +0100

    /api/users/[userId] : GET & PUT pour la page d'édition d'utilisateurs

--- pages/api/users/[userId]/index.js
+++ pages/api/users/[userId]/index.js
@@ -1,5 +1,6 @@
 import withSession from '../../../../lib/session';
 import { query } from '../../../../lib/db';
+import validate from 'validate.js';
 
 async function handler(req, res) {
   const user = req.session.get('user');
@@ -10,11 +11,54 @@ async function handler(req, res) {
       const data = await query(`DELETE FROM users WHERE userId = ?`, req.query.userId);
       res.json({ success: true });
     }
-    else if ((req.method === 'POST' || req.method === 'PUT') && user.userType == 2) {
-      res.send({ success: false });
+    else if ((req.method === 'PUT') && user.userType == 2) {
+      let userCreated = { ...req.body, ...req.query };
+
+      let errors = validate(userCreated, {
+        email: {
+          presence: true,
+          email: true
+        },
+        firstName: {
+          presence: true
+        },
+        lastName: {
+          presence: true
+        },
+        password: {
+          presence: true,
+          length: {
+            minimum: 5
+          }
+        },
+        userType: {
+          presence: true
+        },
+        birthDate: {
+          presence: true
+        }
+      });
+
+      await query(`
+        REPLACE INTO users(userId, firstName, lastName, email, birthDate, userType, groupId)
+        VALUES (?, ?, ?, ?, ?, ?, ?)`, [userCreated.userId, userCreated.firstName, userCreated.lastName, userCreated.email, userCreated.birthDate, userCreated.userType || 0, userCreated?.groupId]);
+
+      res.send({ id: userCreated.userId, success: true });
     }
     else if (req.method === 'GET') {
-      res.send({ success: false });
+      const users = await query(`
+        SELECT userId, email, userType, firstName, lastName, birthDate, groupId, groups.name AS groupName FROM users
+        LEFT OUTER JOIN groups ON groups.id = users.groupId
+        WHERE userId = ?`, req.query.userId);
+
+      if (users.length > 0) {
+        users[0].group = { id: users[0].groupId, name: users[0].groupName };
+        delete users[0].groupId;
+        delete users[0].groupName;
+
+        res.send({ user: users[0], success: true });
+      }
+      else res.status(404).send({ message: 'NOT_FOUND', success: false });
     }
     else res.send({ success: false });
   }
