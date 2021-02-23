403d1fa4f84618ecef206ae8d8ab679b441cbef5
Thomas Cardon <thomas.cardon@pm.me>
2021-02-23 06:06:28 +0100

    Les cours ont maintenant une couleur adaptée

--- components/Schedule.js
+++ components/Schedule.js
@@ -26,6 +26,26 @@ function stringToColor(str) {
   return colour;
 }
 
+function pickTextColorBasedOnBgColorAdvanced(bgColor, lightColor, darkColor) {
+  console.log(bgColor);
+
+  var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  var r = parseInt(color.substring(0, 2), 16); // hexToR
+  var g = parseInt(color.substring(2, 4), 16); // hexToG
+  var b = parseInt(color.substring(4, 6), 16); // hexToB
+  var uicolors = [r / 255, g / 255, b / 255];
+  var c = uicolors.map((col) => {
+    if (col <= 0.03928) {
+      return col / 12.92;
+    }
+    return Math.pow((col + 0.055) / 1.055, 2.4);
+  });
+  var L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+
+  console.log((L > 0.179) ? darkColor : lightColor);
+  return (L > 0.179) ? darkColor : lightColor;
+}
+
 function getLength(number) {
   return number.toString().length;
 }
@@ -89,7 +109,7 @@ export default function Schedule({ classes, children }) {
         <span className={styles.trackSlot} aria-hidden="true" style={{ gridColumn: 'track-6', gridRow: 'tracks' }}>Samedi</span>
 
         {classes.map((x, i) =>
-          <div key={i} onClick={show} className={styles.session} meetingurl={x.meetingurl} style={{ gridColumn: 'track-' + x.day, backgroundColor: x.color || stringToColor(x.name), gridRow: 'time-' + x.start + ' / time-' + x.end }}>
+          <div key={i} onClick={show} className={styles.session} meetingurl={x.meetingurl} style={{ gridColumn: 'track-' + x.day, backgroundColor: x.color || stringToColor(x.name), color: pickTextColorBasedOnBgColorAdvanced(x.color || stringToColor(x.name), 'white', 'black'), gridRow: 'time-' + x.start + ' / time-' + x.end }}>
             <span className={styles.hours}><b>{x.module}</b> - {x.start.slice(0, 2)}:{x.start.slice(2)} - {x.end.slice(0, 2)}:{x.end.slice(2)}</span>
             <p className={styles.name}>{x.name}</p>
             <p className={styles.teacher}>{x.teacher}</p>
