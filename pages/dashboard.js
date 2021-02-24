891eae1ebb481a42647776efd72d1c249daf497b
Thomas Cardon <thomas.cardon@pm.me>
2021-02-24 16:15:24 +0100

    Tableau de bord: placeholder chat lol

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -25,9 +25,20 @@ export default function Dashboard({ user }) {
   else if (posts) {
     content = (
       <div className="cards">
+        {true && (
+          <div className="card card-xl">
+            <h3>Chat:&nbsp;<code style={{ color: 'red' }}>global</code> (Placeholder)</h3>
+            <textarea readOnly style={{ width: '100%', height: '70%', margin: '0', padding: '0' }}></textarea>
+            <div style={{ display: 'flex', width: '100%' }}>
+              <input type="text" placeholder="Ecrire un message" />
+              <button>Envoyer</button>
+            </div>
+          </div>
+        )}
+
         {posts && (
           <div className="card">
-            <h3 className="card-title">Derniers posts</h3>
+            <h3>Derniers posts</h3>
             <ul>
               {posts.data.map((post, i) => <li key={i}>
                 <Link href={"/posts/" + post.id}>
@@ -40,17 +51,7 @@ export default function Dashboard({ user }) {
             </ul>
           </div>
         )}
-        {true && (
-          <div className="card cardd-lg">
-            <h3 className="card-title">Dernières alertes</h3>
-            <ul>
-              {[].map((a, i) => <li key={i}>
-                <HiArrowNarrowRight style={{ verticalAlign: 'middle' }}/>
-                <span>{a.message}</span>
-              </li>)}
-            </ul>
-          </div>
-        )}
+
       </div>
     );
   }
@@ -64,7 +65,7 @@ export default function Dashboard({ user }) {
 
       <style jsx global>{`
         .cards {
-          margin-top: 3em;
+          margin: 3em;
 
           display: grid;
           grid-template-columns: repeat(3, 1fr);
@@ -79,13 +80,25 @@ export default function Dashboard({ user }) {
           padding: 1em;
         }
 
+        .card-lg {
+          min-width: 20vw;
+          min-height: 30vh;
+        }
+
+        .card-xl {
+          min-width: 30vw;
+          min-height: 40vh;
+        }
+
         .card:hover {
           box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
         }
 
-        .card-title {
+        .card h3 {
           font-family: 'Lato';
           font-weight: 300;
+
+          text-align: center;
         }
 
         ul {
