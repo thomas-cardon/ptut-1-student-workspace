f104d62cd7ff5e5d08bf17cf33754f1716020b47
Thomas Cardon <thomas.cardon@pm.me>
2021-02-24 04:55:07 +0100

    Emploi du temps -> suppression logging

--- components/Schedule.js
+++ components/Schedule.js
@@ -27,27 +27,20 @@ function stringToColor(str) {
 }
 
 function pickTextColorBasedOnBgColorAdvanced(bgColor, lightColor, darkColor) {
-  console.log(bgColor);
-
-  var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
-  var r = parseInt(color.substring(0, 2), 16); // hexToR
-  var g = parseInt(color.substring(2, 4), 16); // hexToG
-  var b = parseInt(color.substring(4, 6), 16); // hexToB
-  var uicolors = [r / 255, g / 255, b / 255];
-  var c = uicolors.map((col) => {
+  let color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  let r = parseInt(color.substring(0, 2), 16); // hexToR
+  let g = parseInt(color.substring(2, 4), 16); // hexToG
+  let b = parseInt(color.substring(4, 6), 16); // hexToB
+  let uicolors = [r / 255, g / 255, b / 255];
+  let c = uicolors.map((col) => {
     if (col <= 0.03928) {
       return col / 12.92;
     }
     return Math.pow((col + 0.055) / 1.055, 2.4);
   });
-  var L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+  let L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
 
-  console.log((L > 0.179) ? darkColor : lightColor);
   return (L > 0.179) ? darkColor : lightColor;
-}
-
-function getLength(number) {
-  return number.toString().length;
 }
 
 export default function Schedule({ classes, children }) {
