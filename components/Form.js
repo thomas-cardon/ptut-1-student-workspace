122f305bf0451aa981dfbde56d9d22addaebff0f
Thomas Cardon <thomas.cardon@pm.me>
2021-03-02 23:05:52 +0100

    Form -> suppression css

--- components/Form.js
+++ components/Form.js
@@ -3,14 +3,12 @@ import React from 'react';
 import { useForm, FormProvider } from "react-hook-form";
 import * as Fields from "./FormFields";
 
-import styles from "./Form.module.css";
-
-const Form = React.forwardRef(({ onSubmit, onError, fields, className, style, children, reactHookForm = true }, ref) => {
+const Form = React.forwardRef(({ onSubmit, onError, fields, className = {}, style, children, reactHookForm = true }, ref) => {
   const { handleSubmit, ...methods } = useForm();
 
   return (
     <FormProvider {...methods}>
-      <form ref={ref} className={className ? className : styles.form} style={style ? style : {}} onSubmit={onSubmit} onError={onError}>
+      <form ref={ref} className={className} style={style ? style : {}} onSubmit={onSubmit} onError={onError}>
         {children}
       </form>
     </FormProvider>
