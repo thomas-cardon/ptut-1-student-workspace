74b7d6f1636680dce5087dc5f7663cc03d276c45
Thomas Cardon <thomas.cardon@pm.me>
2021-03-03 23:36:52 +0100

    Fix Form

--- components/Form.js
+++ components/Form.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { useForm, FormProvider } from "react-hook-form";
 import * as Fields from "./FormFields";
 
-const Form = React.forwardRef(({ onSubmit, onError, fields, className = {}, style, children, reactHookForm = true }, ref) => {
+const Form = React.forwardRef(({ onSubmit, onError, fields, className, style, children, reactHookForm = true }, ref) => {
   const { handleSubmit, ...methods } = useForm();
 
   return (
     <FormProvider {...methods}>
-      <form ref={ref} className={className} style={style ? style : {}} onSubmit={onSubmit} onError={onError}>
+      <form ref={ref} className={className} style={style} onSubmit={onSubmit} onError={onError}>
         {children}
       </form>
     </FormProvider>
