abe623c0b8471cf7344cefa19a77c92aec2f248d
Thomas Cardon <thomas.cardon@pm.me>
2021-03-03 23:37:05 +0100

    /api/schedule -> filtrer par professeur

--- pages/api/schedule/index.js
+++ pages/api/schedule/index.js
@@ -5,19 +5,26 @@ async function handler(req, res, session) {
   const user = req.session.get('user');
   if (!user || user.userType == 0) return res.status(401).send('NOT_AUTHORIZED');
 
-  const { filterByGroup } = req.query;
+  const { filterByGroup, filterByTeacher } = req.query;
 
   try {
+    let params = [];
+    if (filterByGroup && filterByTeacher) params = [filterByGroup, filterByTeacher];
+    else if (filterByGroup) params = [filterByGroup];
+    else if (filterByTeacher) params = [filterByTeacher];
+
     const schedule = await query(
       `
-      SELECT schedule.id, start, duration, classId, schedule.groupId, meetingUrl, module, name as moduleName, color as moduleColor, firstName AS teacherFirstName, lastName as teacherLastName, email as teacherEmail FROM schedule
+      SELECT schedule.id, start, duration, classId, schedule.groupId, meetingUrl, module, name as moduleName, color as moduleColor, firstName AS teacherFirstName, lastName as teacherLastName, email as teacherEmail, schedule.teacherId FROM schedule
       INNER JOIN users ON teacherId = users.userId
       INNER JOIN classes ON classId = classes.id
       ${filterByGroup ? 'WHERE schedule.groupId = ?' : ''}
+      ${filterByTeacher ? 'WHERE schedule.teacherId = ?' : ''}
       ORDER BY start ASC
-      `, [user.group.id]);
+      `, params);
 
-    res.send({ schedule, success: true });
+    if (schedule.length > 0) res.send({ schedule, success: true });
+    else res.status(404).send({ message: 'NOT_FOUND', success: false });
   }
   catch (e) {
     res.status(500).json({ message: e.message, success: false });
