cc24fd0db4db4d992ede06626c24a86021c72bdf
Thomas Cardon <thomas.cardon@pm.me>
2021-03-04 00:53:37 +0100

    /posts/create -> meilleur formulaire, devoirs

--- pages/posts/create.js
+++ pages/posts/create.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import Router from 'next/router';
 import dynamic from 'next/dynamic';
 
@@ -14,27 +16,50 @@ import useSWR from 'swr';
 import fetcher from '../../lib/fetchJson';
 import withSession from "../../lib/session";
 
+import { formatDistance } from 'date-fns';
+import { fr } from 'date-fns/locale';
+
 const Editor = dynamic(() => import("../../components/Editor"), { ssr: false });
 
 export default function CreatePostPage({ user, moduleId }) {
-  const { data, error } = useSWR('/api/class/list', fetcher);
+ /*
+  * Variable definitions
+  */
+  const [values, setValues] = useState({ homework: false, title: 'Sans titre', classId: '', courseId: '', homeworkDate: '' });
+
+  const handleInputChange = e => {
+    const {name, value, checked, type } = e.target;
+    setValues({ ...values, [name]: type === 'checkbox' ? checked : value });
+  };
+
+  const { data : classes } = useSWR('/api/class/list', fetcher);
+  const { data : scheduleList } = useSWR(`/api/schedule${user.userType == 1 ? '?filterByTeacher=' + user.userId : ''}`, fetcher);
 
   const { addToast } = useToasts();
+
+ /*
+  * End of variable definitions
+  */
 
   let content = <h1 className={'title'}>Chargement...</h1>;
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    let content = localStorage.getItem('posts.lastSavedState');
+    let body = {
+      title: values.title,
+      content: localStorage.getItem('posts.lastSavedState'),
+      classId: parseInt(values.classId),
+      courseId: values.courseId ? parseInt(values.courseId) : undefined,
+      isHomework: values.homework ? 1 : 0,
+      homeworkDate: values.homework ? values.homeworkDate.slice(0, 19).replace('T', ' ') : undefined
+    };
+
+    if (!body.content) return addToast('Erreur: contenu manquant', { appearance: 'error' });
 
     try {
       const res = await fetch(location.protocol + '//' + location.host + '/api/posts/create', {
-        body: JSON.stringify({
-          title: e.target.title.value,
-          content,
-          classId: e.target.classId.value
-        }),
+        body: JSON.stringify(body),
         headers: { 'Content-Type': 'application/json' },
         method: 'POST'
       });
@@ -61,15 +86,24 @@ export default function CreatePostPage({ user, moduleId }) {
     addToast(errors || 'Une erreur s\'est produite', { appearance: 'error' });
   }
 
-  if (user && data) content = (<>
+  let scheduleSelector = scheduleList && scheduleList.schedule ? (
+    <Fields.FormSelect disabled={values.homework == "1"} label="Attacher à l'emploi du temps ?" name="courseId" onChange={handleInputChange} value={values.courseId} noOption="-- Ne pas attacher à l'emploi du temps --" options={scheduleList.schedule.map(x => { return { option: formatDistance(Date.parse(x.start), new Date(), { locale: fr, addSuffix: true }) + (x.groupName ? ' avec: ' + x.groupName : ''), value: x.id } })} />
+  ) : (
+    <Fields.FormSelect disabled={values.homework == "1"} label="Attacher à l'emploi du temps ?" name="courseId" noOption="-- Aucun cours disponible --" disabled />
+  );
+
+  if (user && classes) content = (<>
     <Title appendGradient="nouveau post">
       Edition d'un
     </Title>
 
     <Highlight title={'Le saviez-vous?'}>Le contenu du post est enregistré sur votre navigateur tant qu'il n'est pas envoyé.</Highlight>
-    <Form onSubmit={onSubmit} onError={onError}>
-      <Fields.FormInput label="Titre du post" id="title" name="title" type="text" placeholder="Titre" required />
-      <Fields.FormSelect label="Cours" id="classId" name="classId" options={data.modules.map(x => { return { option: 'Cours ' + x.module, value: x.id } })} />
+    <Form style={{ width: '60%' }} onSubmit={onSubmit} onError={onError}>
+      <Fields.FormInput label="Titre du post" name="title" type="text" onChange={handleInputChange} value={values.title} placeholder="Titre" required />
+      <Fields.FormSelect label="Matière (Module)" name="classId" onChange={handleInputChange} noOption="-- Sélectionnez un module --" value={values.classId} options={classes.modules.map(x => { return { option: 'Cours ' + x.module, value: x.id } })} required />
+      <Fields.FormCheckbox label="Est-ce un devoir ?" name="homework" inline={true} onChange={handleInputChange} value={values.homework} />
+      <Fields.FormInput disabled={values.homework == "0"} label="Date du devoir" name="homeworkDate" type="datetime-local" onChange={handleInputChange} value={values.homeworkDate} />
+      {scheduleSelector}
       <Fields.FormButton type="submit">Créer un nouveau post</Fields.FormButton>
     </Form>
     <Editor style={{ width: '60%' }} />
