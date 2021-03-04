956f3725ba7bd86c9b440d0f3b06beae7cd3790a
Thomas Cardon <thomas.cardon@pm.me>
2021-03-04 01:27:59 +0100

    Amélioration des paramètres

--- pages/settings.js
+++ pages/settings.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import withSession from "../lib/session";
 import { getAvatar } from '../lib/useUser';
 
@@ -5,15 +7,26 @@ import UserLayout from '../components/UserLayout';
 import Title from '../components/Title';
 
 export default function SettingsPage({ user }) {
+  let permsDesc;
+
+  if (user.userType == 0) permsDesc = 'vous disposez des permissions les plus basiques.';
+  else if (user.userType == 1) permsDesc = 'vous êtes professeur. Vous pouvez créer des posts, ajouter des devoirs, notifier les élèves, etc.';
+  else permsDesc = "vous faites partie de l'administration. Vous avez tous les droits possibles sur le service.";
   return (
     <UserLayout user={user} flex={true}>
       <Title appendGradient={user.firstName}>
         Paramètres utilisateur:
       </Title>
+      <img style={{ borderRadius: '50%', width: '10%', margin: '2em' }} src={getAvatar(user)} />
       <code>
         NOM: {user.firstName} {user.lastName}
       </code>
-      <img style={{ borderRadius: '50%', width: '10%' }} src={getAvatar(user)} />
+      <p style={{ width: '50%', textAlign: 'center' }}>
+        <i>Vous êtes un utilisateur de type {user.userType}, c'est-à-dire que {permsDesc}</i>
+      </p>
+      <Link href="">
+        <a>Mot de passe oublié ?</a>
+      </Link>
     </UserLayout>
   );
 };
