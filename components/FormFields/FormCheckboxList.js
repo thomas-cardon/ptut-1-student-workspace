6a93c7c8b964bf2d0e4517c680ea3e75b3267bc3
Thomas Cardon <thomas.cardon@pm.me>
2021-03-04 02:00:52 +0100

    FormCheckboxList -> ajout d'une key pour éviter des erreurs

--- components/FormFields/FormCheckboxList.js
+++ components/FormFields/FormCheckboxList.js
@@ -6,7 +6,7 @@ import { useFormContext } from 'react-hook-form';
 export default function FormCheckboxList({ label, options, ...rest }) {
   return (<div className={styles.list}>
     <h3>{label}</h3>
-    {options.map(x => (<div>
+    {options.map((x, i) => (<div key={i}>
       <input className={styles.checkbox} {...rest} id={'checkbox-' + (x.id || x.value)} data-id={x.id || x.value} data-label={x.label} type="checkbox" />
       <label htmlFor={'checkbox-' + (x.id || x.value)}>{x.label}</label>
     </div>))}
