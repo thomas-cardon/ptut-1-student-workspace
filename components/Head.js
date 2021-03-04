5e6442a6549e287c2881c7cdfe35eb7220c11717
Thomas Cardon <thomas.cardon@pm.me>
2021-03-04 23:50:50 +0100

    Le site passe en UTF8

--- components/Head.js
+++ components/Head.js
@@ -4,9 +4,10 @@ export default function CustomHead({ title }) {
   return (
     <Head>
       <title>{title || 'Student Workspace'}</title>
-      <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Votre nouvel environnement de travail" />
+      <meta charset="UTF-8" />
+      <link rel="icon" href="/favicon.ico" />
       <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
