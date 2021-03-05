4634289e5e2033e98f1572a5bc255d6795a71d2c
Thomas Cardon <thomas.cardon@pm.me>
2021-03-05 23:26:30 +0100

    Notes: barre de recherche

--- pages/grades/list.js
+++ pages/grades/list.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 import UserLayout from '../../components/UserLayout';
 import Highlight from '../../components/Highlight';
 import Table from '../../components/Table';
 import Title from '../../components/Title';
+
+import * as Fields from "../../components/FormFields";
 
 import {
   contextMenu,
@@ -18,72 +19,79 @@ import {
 
 import use from '../../lib/use';
 import withSession from "../../lib/session";
-
-import { useToasts } from 'react-toast-notifications';
 import { useDarkMode } from 'next-dark-mode';
 
 import { format } from 'date-fns';
 import { fr } from 'date-fns/locale';
 
-export default function ClassListPage({ user, id }) {
-  const { data } = use({ url: '/api/grades' + (id ? '?id=' + id : ''), redirectOnError: '/error' });
-  console.dir(data);
-  const { darkModeActive } = useDarkMode();
-  const { addToast } = useToasts();
-  const router = useRouter();
+import fuzzy from '../../lib/fuzzySearch';
 
-  let content;
+export default function GradesListPage({ user, id }) {
+  /*
+   * Variable definitions
+   */
+   const [query, setQuery] = useState("");
+
+   const { data } = use({ url: '/api/grades' + (id ? '?id=' + id : ''), redirectOnError: '/error' });
+   const { darkModeActive } = useDarkMode();
+
+  let content, users = [];
 
   if (!data) content = <h2 className={'title'}>Chargement</h2>;
   else if (data.data === {}) content = <h2 className={'title'}>Aucune note disponible</h2>;
-  else content = (<>
-    {Object.keys(data.data).map((key, i) => (<div key={key}>
-      <h3 style={{ margin: '1em 0 0 0' }}>
-        <span>{data.data[key][Object.keys(data.data[key])[0]][0].userFirstName} {data.data[key][Object.keys(data.data[key])[0]][0].userLastName}</span>
-        {data.data[key][0]?.userGroupName && (<>
-          <span style={{ margin: '0 0.5em' }}>→</span>
-          <span>
-            Groupe:&nbsp;
-            <i>{data.data[key][0]?.userGroupName}</i>
-          </span>
-        </>)}
-      </h3>
-      <hr style={{ width: '50%', margin: '1em 2em 2em 0' }} />
-      <Table head={['Matière', 'Nom', 'Professeur', 'Note', 'Coef.', 'Absent', 'Appréciations', 'Date']} fixed={true}>
-        {Object.keys(data.data[key]).map((m, i) => (<React.Fragment key={key + '-' + m}>
-          <tr style={{ backgroundColor: darkModeActive ? '#272c34' : '#ddd' }}>
-            <td data-type='subject'>
-              <p style={{ margin: '0' }}>{data.data[key][m][0].subjectModule}</p>
-              <p style={{ fontSize: 'x-small', margin: '0' }}>{data.data[key][m][0].subjectName}</p>
-            </td>
-            <td />
-            <td />
-            <td />
-            <td />
-            <td />
-            <td />
-            <td />
-          </tr>
-          {data.data[key][m].map((note, i) => (<tr key={key + '-' + m + '-' + i}>
-            <td />
-            <td data-type='name'>{note.name}</td>
-            <td data-type='teacher'>{note.teacherFirstName} {note.teacherLastName}</td>
-            <td data-type='value'>{(note.value || 0) + '/' + note.max}</td>
-            <td data-type='coefficient'>{note.coefficient}</td>
-            <td data-type='absent'>{note.wasAbsent === 1 ? '❌' : '✔️'}</td>
-            <td data-type='notes'>{note.notes || 'N/A'}</td>
-            <td data-type='date'>{format(Date.parse(note.date), 'd MMMM yyyy', { locale: fr }) || 'N/A'}</td>
-          </tr>))}
-        </React.Fragment>))}
-      </Table>
-    </div>))}
-  </>);
+  else {
+    users = fuzzy(Object.values(data.data), query);
+    content = (<>
+      {users.map((user, i) => (<div key={user.userId}>
+        <h3 style={{ margin: '1em 0 0 0' }}>
+          <span>{user[Object.keys(user)[0]][0].userFirstName} {user[Object.keys(user)[0]][0].userLastName}</span>
+          {user[0]?.userGroupName && (<>
+            <span style={{ margin: '0 0.5em' }}>→</span>
+            <span>
+              Groupe:&nbsp;
+              <i>{user[0]?.userGroupName}</i>
+            </span>
+          </>)}
+        </h3>
+        <hr style={{ width: '50%', margin: '1em 2em 2em 0' }} />
+        <Table head={['Matière', 'Nom', 'Professeur', 'Note', 'Coef.', 'Absent', 'Appréciations', 'Date']} fixed={true}>
+          {Object.keys(user).map((m, i) => (<React.Fragment key={user.userId + '-' + m}>
+            <tr style={{ backgroundColor: darkModeActive ? '#272c34' : '#ddd' }}>
+              <td data-type='subject'>
+                <p style={{ margin: '0' }}>{user[m][0].subjectModule}</p>
+                <p style={{ fontSize: 'x-small', margin: '0' }}>{user[m][0].subjectName}</p>
+              </td>
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+            {user[m].map((note, i) => (<tr key={user.userId + '-' + m + '-' + i}>
+              <td />
+              <td data-type='name'>{note.name}</td>
+              <td data-type='teacher'>{note.teacherFirstName} {note.teacherLastName}</td>
+              <td data-type='value'>{(note.value || 0) + '/' + note.max}</td>
+              <td data-type='coefficient'>{note.coefficient}</td>
+              <td data-type='absent'>{note.wasAbsent === 1 ? '❌' : '✔️'}</td>
+              <td data-type='notes'>{note.notes || 'N/A'}</td>
+              <td data-type='date'>{format(Date.parse(note.date), 'd MMMM yyyy', { locale: fr }) || 'N/A'}</td>
+            </tr>))}
+          </React.Fragment>))}
+        </Table>
+      </div>))}
+    </>);
+  };
 
   return (
     <UserLayout user={user} flex={true}>
       <Title appendGradient="enregistrées">
         Notes
       </Title>
+      <Fields.FormInput name="search" type="text" onChange={e => setQuery(e.target.value)} value={query} placeholder="Recherchez un utilisateur..." autoComplete="off" disableLogic={true} groupStyle={{ width: '50%' }} />
+      <i style={{ marginBottom: '1em', marginTop: '-0.5em' }}>{users.length} résultats pour {query}...</i>
       {user.userType == 2 && (
         <h3 className={'subtitle'}>
           <Link href="/grades/create">
