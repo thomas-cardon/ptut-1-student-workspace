b53b1c9fe9eb3c7fff251625e0089b571ab1339b
Alex <ARNIAUDAlex04@hotmail.fr>
2021-03-10 18:03:38 +0100

    Sidebar → Nouvelle règle horizontale

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,21 +2,23 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 
 import { HiOutlineMenu } from "react-icons/hi";
+import { useDarkMode } from 'next-dark-mode';
 
 import styles from './Sidebar.module.css';
 import { getAvatar } from '../lib/useUser';
 
 export default function Sidebar({ user, children, active, setActive }) {
+  const { darkModeActive } = useDarkMode();
   return (
     <aside className={styles.sidebar} data-active={active ? 1 : 0}>
       <div className={styles.closeIcon} onClick={() => setActive(!active)}>
         <HiOutlineMenu />
       </div>
       <img style={{ borderRadius: '50%', width: '50%', margin: '2em auto 1em auto', display: 'block' }} src={getAvatar(user)} />
-      <h3 style={{ margin: '0 auto', color: 'white' }}>
+      <h3 style={{ margin: '0 auto', color: '#FAFAFA' }}>
         {user.firstName} {user.lastName}
       </h3>
-      <ul className={styles.list}>
+      <ul className={[styles.list, darkModeActive ? styles['list-dark'] : ''].join(' ')}>
         <Link href="/dashboard">
           <a className={styles.item}>Tableau de bord</a>
         </Link>
@@ -34,7 +36,8 @@ export default function Sidebar({ user, children, active, setActive }) {
         </Link>
 
         {user?.userType == 2 && (<>
-          <hr style={{ width: '85%' }} />
+          <hr style={{ width: '85%', border: 'none', borderBottom: `1px solid ${darkModeActive ? '#282828' : '#FAFAFA'}` }} />
+
           <Link href="/users/list">
             <a className={styles.item}>Liste des cours</a>
           </Link>
