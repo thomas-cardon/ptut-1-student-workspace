4d6df619af40fb8e534e31f46ccbcd254cbba08f
Thomas Cardon <thomas.cardon@pm.me>
2021-03-16 01:56:02 +0100

    Utilisation de Gravatar au lieu des anciens avatars

--- pages/settings.js
+++ pages/settings.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import Gravatar from 'react-gravatar';
+
 import withSession from "../lib/session";
 import { useAvatar } from '../lib/hooks';
 
@@ -9,6 +11,8 @@ import Title from '../components/Title';
 
 import Form from "../components/Form";
 import * as Fields from "../components/FormFields";
+
+import Highlight from '../components/Highlight';
 
 import { useToasts } from 'react-toast-notifications';
 
@@ -50,7 +54,6 @@ export default function SettingsPage({ user }) {
     });
   }
 
-
   if (user.userType == 0) permsDesc = 'vous disposez des permissions les plus basiques.';
   else if (user.userType == 1) permsDesc = 'vous êtes professeur. Vous pouvez créer des posts, ajouter des devoirs, notifier les élèves, etc.';
   else permsDesc = "vous faites partie de l'administration. Vous avez tous les droits possibles sur le service.";
@@ -59,26 +62,23 @@ export default function SettingsPage({ user }) {
       <Title appendGradient={user.firstName}>
         Paramètres utilisateur:
       </Title>
-      <img style={{ borderRadius: '50%', width: '10%', margin: '2em' }} src={useAvatar(user.userId)} />
-      <code>
-        NOM: {user.firstName} {user.lastName}
-      </code>
+      <Highlight>
+        Cliquez&nbsp;<Link href="http://en.gravatar.com/emails/"><a>ici</a></Link>&nbsp;pour accéder à&nbsp;<b>Gravatar</b>&nbsp;et ainsi changer votre photo de profil.
+      </Highlight>
+      <Gravatar size={128} email={user.email} style={{ borderRadius: '50%', margin: '0.5em' }} draggable={false} />
+      <h3 style={{ fontSize: 'xx-large', fontWeight: 'normal', margin: '0 0 1em 0' }}>
+        {user.firstName} <b style={{ color: '#686de0' }}>{user.lastName}</b>
+      </h3>
       <p style={{ width: '50%', textAlign: 'center' }}>
         <i>Vous êtes un utilisateur de type {user.userType}, c'est-à-dire que {permsDesc}</i>
       </p>
 
-      <h2>Changer son avatar</h2>
-      <hr style={{ width: '60%' }} />
-      <Form>
-        <Fields.FormInput label="Votre avatar" id="avatar" name="avatar" type="file" accept="image/x-png,image/gif,image/jpeg" onChange={onAvatarChange} required />
-      </Form>
-
       <h2>Changer son mot de passe</h2>
       <hr style={{ width: '60%' }} />
       <Form onSubmit={onSubmit}>
-        <Fields.FormInput label="Ancien mot de passe" id="oldPassword" name="oldPassword" type="password" required />
-        <Fields.FormInput label="Nouveau mot de passe" id="newPassword" name="newPassword" type="password" required />
-        <Fields.FormButton type="submit">Changer</Fields.FormButton>
+        <Fields.FormInput label="Ancien mot de passe" id="oldPassword" name="oldPassword" type="password" disabled={true} />
+        <Fields.FormInput label="Nouveau mot de passe" id="newPassword" name="newPassword" type="password" disabled={true} />
+        <Fields.FormButton type="submit" disabled={true}>Changer</Fields.FormButton>
       </Form>
     </UserLayout>
   );
