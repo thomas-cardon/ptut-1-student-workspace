178146f17fa1636ee4dfdfb50a80b7aef7251247
Thomas Cardon <thomas.cardon@pm.me>
2021-03-22 11:00:26 +0100

    Paramètres -> API backend mot de passe

--- pages/api/me/index.js
+++ pages/api/me/index.js
@@ -1,8 +1,49 @@
 import withSession from "../../../lib/session";
 
-function handler(req, res) {
-  if (!req?.session?.get('user')) return res.status(401).send({ error: 'NOT_AUTHORIZED', success: false });
-  res.send(req.session.get('user'));
+import { query } from '../../../lib/db';
+import { hash, verify } from '../../../lib/encryption';
+
+async function handler(req, res) {
+  if (req.method === 'PATCH') { // Modification
+    if (!req?.session?.get('user')) return res.status(401).send({ error: 'NOT_AUTHORIZED', success: false });
+    if (!req.body.oldPassword || !req.body.newPassword) return res.status(400).json({ error: 'MISSING_PARAMETERS', success: false });
+
+    try {
+      let u = await query(`
+        SELECT hash
+        FROM users
+        WHERE userId = ?`, [req.session.get('user').userId]);
+
+      if (u.length === 0) return res.status(404).send({ error: 'USER_NOT_EXISTS', success: false });
+
+      const isPasswordVerified = await verify(req.body.oldPassword, u[0].hash);
+      if (!isPasswordVerified) return res.status(400).json({ error: "Vous n'avez pas entré le bon mot de passe.", success: false });
+
+      if (await verify(req.body.newPassword, u[0].hash))
+      return res.status(400).json({ error: "Vous ne pouvez pas redéfinir le même mot de passe.", success: false });
+
+      const results = await query(`
+        UPDATE users
+        SET hash = ?
+        WHERE userId = ?`, [await hash(req.body.newPassword), req.session.get('user').userId]);
+
+      await req.session.destroy();
+      
+      res.writeHead(301, {
+        'cache-control': 'no-store, max-age=0',
+        Location: '/login',
+        'Content-Length': Buffer.byteLength('ok'),
+        'Content-Type': 'text/plain'
+      }).end('ok');
+    }
+    catch(error) {
+      res.status(500).send({ error: error.message, success: false });
+    }
+  }
+  else {
+    if (!req?.session?.get('user')) return res.status(401).send({ error: 'NOT_AUTHORIZED', success: false });
+    res.send(req.session.get('user'));
+  }
 }
 
 export default withSession(handler);
