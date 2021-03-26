bef07ad83b9daf173640747918fd648eca10fa47
Thomas Cardon <thomas.cardon@pm.me>
2021-03-26 20:57:24 +0100

    Nouveau bouton

--- components/FormFields/FormButton.js
+++ components/FormFields/FormButton.js
@@ -1,10 +1,12 @@
 import styles from "./FormButton.module.css";
 import FormGroup from "./FormGroup";
 
-export default function FormButton({ disableStyle = false, ...rest }) {
-  return (
-    <FormGroup disableStyle={disableStyle}>
-      <button className={styles.button} {...rest} />
-    </FormGroup>
-  );
+export default function FormButton({ is, children, icon, ...rest }) {
+  return <button className={[styles.button, styles[is]].join(' ')} {...rest}>
+  {icon ? (
+    <div style={{ display: 'flex' }}>
+      {icon}&nbsp;{children}
+    </div>
+  ) : children}
+  </button>;
 }
