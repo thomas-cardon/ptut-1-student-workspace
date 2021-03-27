b8f688009139ad6318375831038767b61ae4919c
Thomas Cardon <thomas.cardon@pm.me>
2021-03-27 23:20:51 +0100

    Amélioration des diverses input

--- components/FormFields/FormInput.js
+++ components/FormFields/FormInput.js
@@ -3,18 +3,19 @@ import React, { useRef } from 'react';
 import styles from "./FormInput.module.css";
 import FormGroup from "./FormGroup.js";
 
-export default function FormInput({ label, name, disableStyle, groupStyle = {}, disableLogic, type: enumType, ...rest }) {
+export default function FormInput({ label, name, icon, type: enumType, groupStyle, ...rest }) {
   const type = enumType.toLowerCase();
-
-  if (disableStyle) return (
-    <FormGroup label={label} name={name} disableStyle={true}>
-      <input id={name} name={name} type={type} {...rest} />
-    </FormGroup>
-  );
 
   return (
     <FormGroup label={label} name={name} style={groupStyle}>
-      <input className={styles.input} id={name} name={name} type={type} {...rest} />
+      <div className={styles['input-container']}>
+        {icon && (
+          <div className={styles.icon}>
+            {icon}
+          </div>
+        )}
+        <input id={name} name={name} type={type} {...rest} />
+      </div>
     </FormGroup>
   );
 }
