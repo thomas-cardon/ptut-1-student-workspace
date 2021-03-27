0b9e3349dd0db39a413c39559d7a29e7bbc1cfb0
Thomas Cardon <thomas.cardon@pm.me>
2021-03-27 18:57:00 +0100

    /schedule réparations

--- pages/schedule/index.js
+++ pages/schedule/index.js
@@ -1,37 +1,31 @@
+import Title from '../../components/Title';
 import Link from '../../components/Link';
 
 import UserLayout from '../../components/UserLayout';
 import GroupList from '../../components/GroupList';
 
 import Schedule from '../../components/Schedule';
-import { ButtonGroup } from '../../components/FormFields';
+import { ButtonGroup, FormButton } from '../../components/FormFields';
 
 import { useSchedule } from '../../lib/hooks';
 import withSession from "../../lib/session";
 
 import { lightFormat, getDay } from 'date-fns';
 
-import { HiPlus } from "react-icons/hi";
+import { HiPlusCircle } from "react-icons/hi";
 
 export default function SchedulePage({ user, selectedWeek }) {
   const { data : schedule } = useSchedule(user);
 
   let content = <h2 className={'title'}>Chargement</h2>;
   let data = (schedule || []).map(x => {
-    let start = new Date(x.start), end = new Date(start.getTime() + x.duration * 60 * 1000);
-
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-      start.setHours(start.getHours() + 1);
-      end.setHours(end.getHours() + 1);
-    }
-
-    const startIso = start.toISOString(), endIso = end.toISOString();
+    const start = new Date(x.start), end = new Date(x.end);
 
     return {
       id: x.id,
-      day: getDay(start),
-      start: startIso.slice(11, 16).replace(':', ''),
-      end: endIso.slice(11, 16).replace(':', ''),
+      day: getDay(x.start),
+      start: start.toLocaleTimeString().slice(0, 5).replace(':', ''),
+      end: end.toLocaleTimeString().slice(0, 5).replace(':', ''),
       module: x.module,
       name: x.subjectName,
       room: x.room,
@@ -52,24 +46,23 @@ export default function SchedulePage({ user, selectedWeek }) {
 
   return (
     <UserLayout user={user} flex={true} header={<>
-      <Link href={{ pathname: '/schedule/edit' }}>
-        <button><HiPlus /></button>
-      </Link>
-      <ButtonGroup>
+      <Title appendGradient="temps" button={user.userType > 0 ?
+        <Link href={{ pathname: '/schedule/edit' }}>
+          <FormButton is="action" icon={<HiPlusCircle />}>Ajouter</FormButton>
+        </Link> : <></>}>
+        Emploi du
+      </Title>
+      <ButtonGroup style={{ width: 'fit-content', margin: 'auto'}}>
         {[-3, -2, -1, 0, 1, 2, 3].map((e, i) => {
           let week = Math.ceil((todaydate.getDay() + 1 + numberOfDays) / 7);
           return (
             <Link key={'week-' + i} href={{ pathname: '/schedule', query: { selectedWeek: week + e } }}>
-
                 <button key={i} disabled={selectedWeek ? (week + e) == selectedWeek : e === 0}>S-{week + e}</button>
-
             </Link>);
         })}
       </ButtonGroup>
     </>}>
-      <div className={'grid'} style={{ width: '100%', margin: '0' }}>
-        {content}
-      </div>
+      {content}
     </UserLayout>
   );
 };
