36a370a624070591408945319bb39a06d53c0847
Thomas Cardon <thomas.cardon@pm.me>
2021-03-27 23:53:48 +0100

    Amélioration des input checkbox / select

--- components/FormFields/FormCheckbox.js
+++ components/FormFields/FormCheckbox.js
@@ -3,7 +3,7 @@ import FormGroup from "./FormGroup.js";
 
 export default function FormCheckbox({ label, inline, name, ...rest }) {
   return (
-    <FormGroup label={label} name={name} inline={inline}>
+    <FormGroup label={label} name={name} inline={inline} style={styles.group}>
       <input className={styles.checkbox} id={name} name={name} ref={register({ required: rest.required })} type="checkbox" {...rest} />
     </FormGroup>
   );
--- components/FormFields/FormSelect.js
+++ components/FormFields/FormSelect.js
@@ -2,21 +2,28 @@ import FormGroup from "./FormGroup.js";
 
 import styles from "./FormSelect.module.css";
 
-export default function FormSelect({ label, options, noOption, name, ...rest }) {
+export default function FormInput({ label, name, icon, options, noOption, groupStyle, ...rest }) {
   if (!options) return null;
 
   return (
-    <FormGroup label={label} name={name}>
-      <select className={styles.input} id={name} name={name} {...rest}>
-        {noOption && (
-          <option value="">{noOption}</option>
+    <FormGroup label={label} name={name} style={groupStyle}>
+      <div className={styles['input-container']}>
+        {icon && (
+          <div className={styles.icon}>
+            {icon}
+          </div>
         )}
-        {options.map(({ option, ...opt }, index) => (
-          <option key={index} {...opt}>
-            {option}
-          </option>
-        ))}
-      </select>
+        <select id={name} name={name} {...rest} >
+          {noOption && (
+            <option value="">{noOption}</option>
+          )}
+          {options.map(({ option, ...opt }, index) => (
+            <option key={index} {...opt}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
     </FormGroup>
   );
 }
