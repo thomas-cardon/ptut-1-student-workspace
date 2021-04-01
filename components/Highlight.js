efa7ef3e5ee2505037236a4e6c2eb05f77b41180
Thomas Cardon <thomas.cardon@pm.me>
2021-04-01 21:35:13 +0200

    Highlight -> style perso

--- components/Highlight.js
+++ components/Highlight.js
@@ -1,8 +1,8 @@
 import styles from './Highlight.module.css';
 
-export default function Highlight({ title = 'Important', icon = '☝️', children }) {
+export default function Highlight({ title = 'Important', icon = '☝️', style, children }) {
   return (
-    <div className={styles.block}>
+    <div className={styles.block} style={style}>
       <div className={styles.icon}>{icon}</div>
       <div className={styles.title}>{title}</div>
       {children}
