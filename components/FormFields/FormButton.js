e445a608f28a5455ad35f2232be60110b1e48cca
Thomas Cardon <thomas.cardon@pm.me>
2021-04-02 00:29:17 +0200

    Refonte responsive

--- components/FormFields/FormButton.js
+++ components/FormFields/FormButton.js
@@ -6,7 +6,7 @@ import FormGroup from "./FormGroup";
 export default function FormButton({ is, children, icon, ...rest }) {
   return <button className={[styles.button, styles[is]].join(' ')} {...rest}>
   {icon && children ? (
-    <div style={{ display: 'flex', alignItems: 'center', weight: '100%', height: '100%' }}>
+    <div className={styles.content}>
       {icon}&nbsp;{children}
     </div>
   ) : (children || icon)}
--- components/Title.js
+++ components/Title.js
@@ -1,7 +1,7 @@
 import * as defaultStyles from './Title.module.css';
 import { useDarkMode } from 'next-dark-mode';
 
-export default function Title({ appendGradient, button, style = {}, children }) {
+export default function Title({ appendGradient, subtitle, button, style = {}, children }) {
   const { darkModeActive } = useDarkMode();
 
   return (
@@ -9,6 +9,10 @@ export default function Title({ appendGradient, button, style = {}, children }) 
       <h1 className={[defaultStyles.title, darkModeActive ? defaultStyles.dark : ''].join(' ')}>
         {children}&nbsp;
         <span className={defaultStyles.gradient}>{appendGradient}</span>
+        {subtitle && (<>
+          <br />
+          <span className={defaultStyles.subtitle}>{subtitle}</span>
+        </>)}
       </h1>
       {button || <></>}
     </div>
