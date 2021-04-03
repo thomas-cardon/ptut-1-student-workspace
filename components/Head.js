0b63154bd8fec352fac48ea4b6c1b8f00fb5790b
Thomas Cardon <thomas.cardon@pm.me>
2021-04-03 18:31:32 +0200

    PWA theme color

--- components/Head.js
+++ components/Head.js
@@ -7,6 +7,7 @@ export default function CustomHead({ title }) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="Votre nouvel environnement de travail" />
       <meta charSet="UTF-8" />
+      <meta name="theme-color" content="#5575e7" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="manifest" href="/site.webmanifest" />
       <script src="https://js.pusher.com/beams/1.0/push-notifications-cdn.js"></script>
