05cd176b2df7aa6415a43c6327d702f8fc10c978
Thomas Cardon <thomas.cardon@pm.me>
2021-04-13 02:54:31 +0200

    PWA upgrade

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  },
   future: {
     webpack5: true,
   },
@@ -16,4 +21,4 @@ module.exports = {
       }
     ];
   }
-}
+});
