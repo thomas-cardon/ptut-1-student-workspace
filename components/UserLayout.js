8142f2a770f6a8107461ac2fefc3fe67bc18a1f7
Thomas Cardon <thomas.cardon@pm.me>
2021-04-19 23:28:00 +0200

    Premiers travaux sur l'inscription

--- components/UserLayout.js
+++ components/UserLayout.js
@@ -58,44 +58,6 @@ export default function UserLayout({ title, user, children, header, flex = true,
 
   return (<>
     <Head />
-    {darkModeActive ? <style jsx global>{`
-        :root {
-          --color-button-text: #fff;
-          --color-primary-100: #dee3ea;
-          --color-primary-200: #b2bdcd;
-          --color-primary-300: #5d7290;
-          --color-primary-600: #323d4d;
-          --color-primary-700: #242c37;
-          --color-primary-800: #151a21;
-          --color-primary-900: #0b0e11;
-          --color-secondary-washed-out: #879eed;
-          --color-secondary: #5575e7;
-          --color-accent-glow: rgba(253,77,77,0.3);
-          --color-accent: #fd4d4d;
-          --color-accent-hover: #fd6868;
-          --color-accent-disabled: #f5bfbf;
-        }
-      `}</style> : <style jsx global>{`
-        :root {
-            font-size: 16px;
-
-            --color-button-text: #fff;
-            --color-primary-100: #151a21;
-            --color-primary-200: #242c37;
-            --color-primary-300: #323d4d;
-            --color-primary-600: #5d7290;
-            --color-primary-700: #b2bdcd;
-            --color-primary-800: #dee3ea;
-            --color-primary-900: #fff;
-
-            --color-secondary-washed-out: #879eed;
-            --color-secondary: #5575e7;
-            --color-accent-glow: rgba(253,77,77,0.3);
-            --color-accent: #fd4d4d;
-            --color-accent-hover: #fd6868;
-            --color-accent-disabled: #f5bfbf;
-        }
-      `}</style>}
     <main className={[styles.main, darkModeActive ? styles.dark : ''].join(' ')} {...rest}>
       <Sidebar user={user}></Sidebar>
 
