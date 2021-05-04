c06aff368a37a5d46f99e733c6aab3bd64ea06b6
Thomas Cardon <thomas.cardon@pm.me>
2021-05-04 17:32:37 +0200

    BasicLayout fix

--- components/BasicLayout.js
+++ components/BasicLayout.js
@@ -1,10 +1,8 @@
-import Head from './Head';
 import styles from './BasicLayout.module.css';
 
 export default function BasicLayout({ title, disableBackground, children, rest }) {
   return (
     <main className={[styles.main, disableBackground ? styles.nobg : ''].join(' ')} {...rest}>
-      <Head title={title} />
       {children}
     </main>
   );
