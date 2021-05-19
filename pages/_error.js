bc8b6568bf337fd41148dd50b14c06b4fb101fec
Thomas Cardon <thomas.cardon@pm.me>
2021-05-19 12:25:47 +0200

    Page erreur +

--- pages/_error.js
+++ pages/_error.js
@@ -4,7 +4,7 @@ import Title from '../components/Title';
 
 import useUser from '../lib/useUser';
 
-function Error({ statusCode }) {
+function Error({ statusCode, ...rest }) {
   const { user } = useUser({ redirectTo: '/login' });
 
   return (
@@ -17,13 +17,18 @@ function Error({ statusCode }) {
           ? `An error ${statusCode} occurred on server`
           : 'An error occurred on client'}
       </Highlight>
+      <pre>
+        <code>
+          {typeof rest === 'object' ? JSON.stringify(rest) : rest}
+        </code>
+      </pre>
     </UserLayout>
   );
 };
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
+  return { ...err, statusCode };
 }
 
 export default Error;
