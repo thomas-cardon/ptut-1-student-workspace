a318e031e903e7d99e035cb3ed4f5ff192a48cd9
Thomas Cardon <thomas.cardon@pm.me>
2021-05-24 12:29:45 +0200

    Link refactoring

--- components/Link.js
+++ components/Link.js
@@ -1,11 +1,10 @@
 import Link from 'next/link';
 import styles from './Link.module.css';
 
-export default function Title({ className, style, children, target, ...rest }) {
-
+export default function CustomLink({ id, className, style, children, target, ...rest }) {
   return (
     <Link {...rest}>
-      <a className={[styles.link, className].join(' ')} style={style} target={target}>
+      <a id={id} className={[styles.link, className].join(' ')} style={style} target={target}>
         {children}
       </a>
     </Link>
