5550cd6f1b80e111b5c559c02f3f4bba6f671aec
Thomas Cardon <thomas.cardon@pm.me>
2021-05-24 13:13:40 +0200

    Homework suppression logs

--- components/Homework.js
+++ components/Homework.js
@@ -37,8 +37,6 @@ export default function Homework({ user, groupId }) {
     return <></>;
   }
 
-  console.dir(Object.entries(homework));
-
   return (
     <div className={styles.content}>
       {data && user?.group ? (<>
