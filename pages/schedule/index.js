06aaee00d9d58bc5d4750337108494b010fec18d
Thomas Cardon <thomas.cardon@pm.me>
2021-06-02 22:59:42 +0200

    Emploi du temps: désactivation affichage personnalisé

--- pages/schedule/index.js
+++ pages/schedule/index.js
@@ -111,7 +111,7 @@ export default function SchedulePage() {
             <Separator />
           </>)}
           <Submenu disabled={true} label="👥 Affichage">
-            {user?.school && user?.degree && getSchoolYears(user).filter(group => group === "Prof" ? (user.userType > 0 || user?.group?.name === 'Professeur') : true).map(group => <Item id={group} key={group} onClick={handleItemClick}>{group === year ? '✅ ' : ''}{group}</Item>)}
+            {user?.school && user?.degree && [].map(group => <Item id={group} key={group} onClick={handleItemClick}>{group === year ? '✅ ' : ''}{group}</Item>)}
           </Submenu>
         </Menu>
         <ButtonGroup>
